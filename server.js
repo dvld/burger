1680edf833e6708a1aea37318fb2eac16b937dd5
dvld <nikoroberts620@gmail.com>
2019-01-07 10:41:46 -0600

    completed server js file

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 
-// dependencies
 var express = require('express');
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+// listener
+app.listen(PORT, function () {
+  console.log('Listening on port: ' + PORT);
+});
